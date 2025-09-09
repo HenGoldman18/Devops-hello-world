d3f6e8f7dfd8eb5ca3ae076146d10ff4b6415692
Goldman <4work6243@gmail.com>
2025-09-09 18:02:18 +0300

    The test is over. roll back to the original code

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app =Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello DevOps! this is test for change in Github."
+    return "Hello DevOps!"
 if __name__ == "__Main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
 
