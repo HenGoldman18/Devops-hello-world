77591849b939ce45ee66c2163fb0203f764fda84
Goldman <4work6243@gmail.com>
2025-09-09 17:59:17 +0300

    add new line for test to app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 app =Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello DevOps!"
+    return "Hello DevOps! this is test for change in Github."
 if __name__ == "__Main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
+
